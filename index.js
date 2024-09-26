8c7fdcedd093a9213bf20a8e8fcea1958a9ca440
Prince Kumar Singhal <singhalprince111@gmail.com>
2024-09-26 17:12:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
-const container = document.getElementById("containerEle");
+const container = document.getElementById('containerEle');
 
-const { DoppleXR } = await import(
-	"https://builds.dopple.io/packages/dopple-sdk@rc/dopple-sdk.js"
-);
+const { DoppleXR } = await import('https://builds.dopple.io/packages/dopple-sdk@rc/dopple-sdk.js');
+
+const sessionId = window.crypto.randomUUID();
+
+if (!localStorage.getItem('ROARR_LOG')) {
+  localStorage.setItem('ROARR_LOG', 'true');
+}
+
+DD_LOGS.init({
+  clientToken: 'pub282819eaab8c4828d216ccb15fae6ad8',
+  site: 'datadoghq.com',
+  service: 'prototype',
+  env: 'stage',
+  forwardErrorsToLogs: true,
+  sessionSampleRate: 100,
+});
 
 const dopple = new DoppleXR({
-	container,
-	selection: {},
-	productVersion: 2,
-	owner: "dopple",
-	workspace: "bill-company-ws",
-	projectName: "arrow-boat-fixed-2",
-	logNamespace: "sdk",
-	sessionId: "1",
+  container,
+  selection: {},
+  productVersion: 2,
+  owner: 'dopple',
+  workspace: 'bill-company-ws',
+  projectName: 'arrow-boat-fixed-2',
+  logNamespace: 'sdk',
+  sessionId,
 });
+
+const fileLogWriter = (log) => {
+  DD_LOGS.logger.info(log);
+};
+
+dopple.addLogWriter(fileLogWriter);
 
 await dopple.load();
 
 const selected = Object.entries(dopple.matrix.choices).reduce(
-	(acc, [key, value]) => {
-		acc[key] = Object.keys(value.options)[0];
-		return acc;
-	},
+  (acc, [key, value]) => {
+    acc[key] = Object.keys(value.options)[0];
+    return acc;
+  },
 
-	{},
+  {},
 );
 
 await dopple.updateSelection(selected);
@@ -33,56 +52,56 @@ dopple.resize({ trackParentSize: true });
 dopple.run();
 
 {
-	function getChoices() {
-		return Object.keys(dopple.matrix.choices);
-	}
+  function getChoices() {
+    return Object.keys(dopple.matrix.choices);
+  }
 
-	function getOptions(name) {
-		return Object.keys(dopple.matrix.choices[name].options);
-	}
+  function getOptions(name) {
+    return Object.keys(dopple.matrix.choices[name].options);
+  }
 
-	function createDropdown(name) {
-		const select = document.createElement("select");
-		select.name = name;
+  function createDropdown(name) {
+    const select = document.createElement('select');
+    select.name = name;
 
-		const options = getOptions(name);
+    const options = getOptions(name);
 
-		for (const option of options) {
-			const opt = document.createElement("option");
-			opt.value = option;
-			opt.textContent = option;
-			option === selected[name] && opt.setAttribute("selected", "selected");
-			select.appendChild(opt);
-		}
+    for (const option of options) {
+      const opt = document.createElement('option');
+      opt.value = option;
+      opt.textContent = option;
+      option === selected[name] && opt.setAttribute('selected', 'selected');
+      select.appendChild(opt);
+    }
 
-		select.addEventListener("change", handleSelectionChange);
+    select.addEventListener('change', handleSelectionChange);
 
-		return select;
-	}
+    return select;
+  }
 
-	async function handleSelectionChange(event) {
-		const selectedValue = event.target.value;
-		selected[event.target.name] = selectedValue;
-		console.log("Selection changed to:", selectedValue);
+  async function handleSelectionChange(event) {
+    const selectedValue = event.target.value;
+    selected[event.target.name] = selectedValue;
+    console.log('Selection changed to:', selectedValue);
 
-		await dopple.updateSelection(selected);
-	}
+    await dopple.updateSelection(selected);
+  }
 
-	function populateChoices() {
-		const choicesList = document.getElementById("choicesList");
-		const choices = getChoices();
+  function populateChoices() {
+    const choicesList = document.getElementById('choicesList');
+    const choices = getChoices();
 
-		for (const choice of choices) {
-			const li = document.createElement("li");
-			li.textContent = `${choice}: `;
-			li.id = "list-group-item";
+    for (const choice of choices) {
+      const li = document.createElement('li');
+      li.textContent = `${choice}: `;
+      li.id = 'list-group-item';
 
-			const dropdown = createDropdown(choice);
-			li.appendChild(dropdown);
+      const dropdown = createDropdown(choice);
+      li.appendChild(dropdown);
 
-			choicesList.appendChild(li);
-		}
-	}
+      choicesList.appendChild(li);
+    }
+  }
 
-	populateChoices();
+  populateChoices();
 }
